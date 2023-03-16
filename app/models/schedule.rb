6d4b78d9d05e8f10cd2dfe9b249674c159ba756e
andrew <kondratev-28@mail.ru>
2023-03-16 14:11:30 +0700

    представление schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,16 @@
 class Schedule < ApplicationRecord
+  DAYS_OF_WEEK = {
+    1 => "Понедельник",
+    2 => "Вторник",
+    3 => "Среда",
+    4 => "Четверг",
+    5 => "Пятница",
+    6 => "Суббота"
+  }.freeze
+
+  has_many :lessons
+
+  def wday
+    DAYS_OF_WEEK[weeks_day]
+  end
 end
