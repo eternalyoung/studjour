b3ec2932076f3f02634cd902e5f95c283335d946
andrew <kondratev-28@mail.ru>
2023-03-18 13:55:09 +0700

    Логика создания урока

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: %i[ show edit update destroy ]
+  before_action :set_group, only: %i[show edit update destroy]
 
   # GET /groups
   def index
@@ -7,8 +7,7 @@ class GroupsController < ApplicationController
   end
 
   # GET /groups/1
-  def show
-  end
+  def show; end
 
   # GET /groups/new
   def new
@@ -16,8 +15,7 @@ class GroupsController < ApplicationController
   end
 
   # GET /groups/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /groups
   def create
@@ -46,13 +44,14 @@ class GroupsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def group_params
-      params.require(:group).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,25 @@ class Lesson < ApplicationRecord
   belongs_to :subject
   belongs_to :teacher
   belongs_to :room
+
+  validates :number, presence: true
+  validates :number, numericality: { only_integer: true }
+  validates :number, numericality: { greater_than: 0 }
+  validates :number, numericality: { less_than: 8 }
+  validates :number, uniqueness: { scope: :schedule_id }, unless: -> { adjacent }
+  validates :adjacent, inclusion: [true, false]
+  validates :schedule, presence: true
+  validates :subject, presence: true
+  validates :teacher, presence: true
+  validates :room, presence: true
+  validates :got_changes, inclusion: [true, false]
+  validate :adjacent_lesson_uniqueness
+
+  private
+
+  def adjacent_lesson_uniqueness
+    return unless adjacent? && schedule.lessons.where(number:, adjacent: true).exists?
+
+    errors.add(:adjacent, "can only be set for one lesson in the schedule")
+  end
 end
