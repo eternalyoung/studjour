e4e9413a1a4b33bf4d62e111bff2733443ab6ba0
andrew <kondratev-28@mail.ru>
2023-06-14 21:57:41 +0700

    controller fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:edit, :destroy]
+  before_action :set_event, only: %i[edit destroy update]
 
   def index
     @events = Event.all
@@ -16,15 +16,23 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
 
     if @event.save
-      redirect_to events_path, notice: "Event was successfully created."
+      redirect_to events_path, notice: "Мероприятие успешно создано."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def update
+    if @event.update(event_params)
+      redirect_to events_path, notice: "Мероприятие успешно обновлено."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @event.destroy
-    redirect_to events_url, notice: "Event was successfully destroyed."
+    redirect_to events_url, notice: "Мероприятие успешно удалено."
   end
 
   private
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :subscriptions, dependent: :destroy
+
+  validates :description, length: {maximum: 300}
 end
