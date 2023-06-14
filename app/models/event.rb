c62eb025f782fbaa80e0a5575edf320a400e1958
andrew <kondratev-28@mail.ru>
2023-06-15 00:20:30 +0700

    event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
 
+  validates :title, presence: true
+  validates :title, length: {minimum: 3}
+  validates :description, length: {minimum: 30}
   validates :description, length: {maximum: 300}
 end
